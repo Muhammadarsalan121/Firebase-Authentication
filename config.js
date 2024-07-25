300505475cec80a136000bb7b963674a98496292
muhammadarsalan121 <arsalanshahid486@gmail.com>
2024-07-25 22:26:21 +0500

    Firebase Authentication Login App

--- config.js
+++ config.js
@@ -1,21 +1,25 @@
-  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyADCWpp3JMpgXKqxUk3y1tlPoYLNqDUfNE",
-    authDomain: "authenticate-firebase121.firebaseapp.com",
-    projectId: "authenticate-firebase121",
-    storageBucket: "authenticate-firebase121.appspot.com",
-    messagingSenderId: "342164087266",
-    appId: "1:342164087266:web:fdbbb7579ada362551d9bb",
-    measurementId: "G-JNEDJSZCXT"
-  };
-
-  // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+ 
+ 
+  
+   // Import the functions you need from the SDKs you need
+   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+   import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+   // TODO: Add SDKs for Firebase products that you want to use
+   // https://firebase.google.com/docs/web/setup#available-libraries
+ 
+   // Your web app's Firebase configuration
+   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   const firebaseConfig = {
+     apiKey: "AIzaSyC980WH59p1HMn5JlWwjsyfOMeXzBq0wFA",
+     authDomain: "authenticate-firebase1212.firebaseapp.com",
+     projectId: "authenticate-firebase1212",
+     storageBucket: "authenticate-firebase1212.appspot.com",
+     messagingSenderId: "466349963538",
+     appId: "1:466349963538:web:a8a96571cdbd6eea2e9bcb",
+     measurementId: "G-WLM10ECMGD"
+   };
+ 
+   // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
  export const auth = getAuth(app);
